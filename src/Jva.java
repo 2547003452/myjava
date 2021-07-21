601b8bf7dca3bca29615c9b6faaaf0ef291b68f2
clm <2547003452@qq.com>
2021-07-21 22:23:34 +0800

    mycah

--- src/Jva.java
+++ src/Jva.java
@@ -1,6 +1,6 @@
 public class Jva {
     public static void main(String[] args) {
         System.out.println("傻逼");
-        System.out.println("sssaa");
+        System.out.println("sssaass");
     }
 }
